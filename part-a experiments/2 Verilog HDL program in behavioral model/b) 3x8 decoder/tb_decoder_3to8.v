a4c27793fdae483e0012bf59ea114fcb67c98c82
hdl-eda <127012447+hdl-eda@users.noreply.github.com>
2023-03-06 15:10:49 +0530

    Create tb_decoder_3to8.v

---
+++ part-a experiments/2 Verilog HDL program in behavioral model/b) 3x8 decoder/tb_decoder_3to8.v
@@ -0,0 +1,22 @@
+module testbench;
+
+    // Instantiate the module under test
+    decoder_3to8 dut (
+        .in(in),
+        .out(out)
+    );
+    
+    // Define the input and output signals
+    reg [2:0] in;
+    wire [7:0] out;
+    
+    // Initialize the inputs
+    initial begin
+        in = 3'b000;
+    end
+    
+    // Stimulate the inputs and check the outputs
+    always #10 in = in + 1;
+    always @* $display("Input: %b, Output: %b", in, out);
+    
+endmodule
