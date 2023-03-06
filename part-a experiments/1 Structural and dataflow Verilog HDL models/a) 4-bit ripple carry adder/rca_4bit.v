ea9388575e08b41fb2646524d662031106341efe
hdl-eda <127012447+hdl-eda@users.noreply.github.com>
2023-03-06 10:31:49 +0530

    Update rca_4bit.v

--- part-a experiments/1 Structural and dataflow Verilog HDL models/a) 4-bit ripple carry adder/rca_4bit.v
+++ part-a experiments/1 Structural and dataflow Verilog HDL models/a) 4-bit ripple carry adder/rca_4bit.v
@@ -1,19 +1,20 @@
 //Design of 4 Bit Adder using 4 Full adder (Structural Modeling Style)
 
 
-module adder_4bit ( a ,b ,sum ,carry );
+module adder_4bit (a,b,cin,sum,cout);
 
-output [3:0] sum ;
-output carry ;
+output [3:0]sum;
+output cout;
 
-input [3:0] a ;
-input [3:0] b ; 
+input [3:0]a;
+input [3:0]b;
+input cin;
 
-wire [2:0]s;
+  wire [2:0]c;
 
-full_adder u0 (a[0],b[0],1'b0,sum[0],s[0]);
-full_adder u1 (a[1],b[1],s[0],sum[1],s[1]);
-full_adder u2 (a[2],b[2],s[1],sum[2],s[2]);
-full_adder u3 (a[3],b[3],s[2],sum[3],carry);
+  full_adder u0 (a[0],b[0],cin,sum[0],c[0]);
+  full_adder u1 (a[1],b[1],c[0],sum[1],c[1]);
+  full_adder u2 (a[2],b[2],c[1],sum[2],c[2]);
+  full_adder u3 (a[3],b[3],c[2],sum[3],cout);
 
 endmodule
