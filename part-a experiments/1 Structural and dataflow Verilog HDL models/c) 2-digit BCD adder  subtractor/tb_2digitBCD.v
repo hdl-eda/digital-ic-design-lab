07dca85cb798dc7ad3e31af944a3fd162d0d0bb2
hdl-eda <127012447+hdl-eda@users.noreply.github.com>
2023-03-06 10:07:37 +0530

    Update and rename tb.v to tb_2digitBCD.v

--- part-a experiments/1 Structural and dataflow Verilog HDL models/c) 2-digit BCD adder  subtractor/tb_2digitBCD.v
+++ part-a experiments/1 Structural and dataflow Verilog HDL models/c) 2-digit BCD adder  subtractor/tb_2digitBCD.v
@@ -2,54 +2,16 @@
 
 module tb;
 
-/*
-  reg [3:0]A,B;
-  reg CIN,M;
-  wire [3:0]F;
-  wire COUT;
-  
-  add_sub  dut(A, B, CIN, M, F, COUT);
-  */
+
   reg [3:0]A1,B1;
   reg [7:4]A2,B2;
   reg CIN,M;
   wire [3:0]F1;
   wire [7:4]F2;
   wire COUT;
-  /*
-  initial begin
   
-  
-       CIN=0;
-  
-       A=5; B=9;  M=0;    // expected 14
-  
-  #10  A=5; B=9; M=0;     // expected 14
-  
-  #10  A=9; B=9; M=0;       // expected 18
-  
-  #10  A=1; B=9; M=0;     // expected 10
-  
-  #10  A=9; B=9; M=1;     // expected 0
-  
-  #10  A=9; B=5; M=1;       // expected 4
-  
-  #10  A=5; B=9; M=1;      // -4
-  
-  #10  A=5; B=2; M=1;     // expected 3
-    
-  #10  A=9; B=1; M=0;      // expected 8
-  
-  #10;
-  
-  $finish;
-  
-  end
-  
-  initial
-  $monitor($time, "ns A=%0d   B=%0d    CIN=%b M=%b COUT=%b  F=%0d   ", A,B,CIN,M,COUT,F);
-  */
   add_sub_2digit dut(A2,A1,B2,B1,CIN,M,F2,F1,COUT);
+ 
   initial begin
   
   
