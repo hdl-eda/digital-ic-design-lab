63cc54acb5f1f5a76dba870083e87f2253ed0131
hdl-eda <127012447+hdl-eda@users.noreply.github.com>
2023-03-06 15:26:58 +0530

    Create tb_encoder8x3.v

---
+++ part-a experiments/2 Verilog HDL program in behavioral model/c) 8x3 encoder/tb_encoder8x3.v
@@ -0,0 +1,22 @@
+module testbench;
+
+    // Instantiate the module under test
+    encoder_8x3 dut (
+        .in(in),
+        .out(out)
+    );
+    
+    // Define the input and output signals
+    reg [7:0] in;
+    wire [2:0] out;
+    
+    // Initialize the inputs
+    initial begin
+        in = 8'b00000001;
+    end
+    
+    // Stimulate the inputs and check the outputs
+    always #10 in = in << 1;
+    always @* $display("Input: %b, Output: %b", in, out);
+    
+endmodule
