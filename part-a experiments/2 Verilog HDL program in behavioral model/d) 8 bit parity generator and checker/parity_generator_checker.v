94ccad5a010a2e7c00860d75bff7648b3fa047d1
hdl-eda <127012447+hdl-eda@users.noreply.github.com>
2023-03-08 13:25:28 +0530

    Create parity_generator_checker.v

---
+++ part-a experiments/2 Verilog HDL program in behavioral model/d) 8 bit parity generator and checker/parity_generator_checker.v
@@ -0,0 +1,52 @@
+module parity_generator_checker (
+  input [7:0] data_in,
+  input odd_parity_control,
+  input even_parity_control,
+  input odd_parity_check_control,
+  input even_parity_check_control,
+  output [8:0] data_out_with_parity_bit,
+  output parity_out,
+  output reg parity_error
+);
+
+  reg parity_bit;
+assign parity_out = parity_bit;
+  
+  always @* begin  // Parity generator
+    if (odd_parity_control) begin
+      parity_bit = ~^data_in;
+    end else if (even_parity_control) begin
+      	  parity_bit = ^data_in;
+    end else if (odd_parity_check_control) 
+	begin
+
+      if (~^{data_in,parity_bit})
+	  begin
+	  
+	    $display("For ODD From DUT parity_bit is %b",parity_bit);
+		
+        parity_error = 1'b1;
+	  end
+    end	  
+	else if (even_parity_check_control) 
+	begin
+
+      if (^{data_in,parity_bit})
+	  begin
+	  
+	    $display("For EVEN From DUT parity_bit is %b",parity_bit);
+		
+        parity_error = 1'b1;
+      end
+    end
+	else 
+	begin
+        parity_error = 1'b0;
+		parity_bit=1'b0;
+    end
+    end
+
+  // Output data with parity bit
+  assign data_out_with_parity_bit = {data_in, parity_bit};
+
+endmodule
