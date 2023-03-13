a68f7346635143d367bacf51476893fc5de5905e
Mohammed Zakir Hussain <127012447+hdl-eda@users.noreply.github.com>
2023-03-13 15:05:27 +0530

    Create counter_down4bit.v

---
+++ part-a experiments/4 Verilog HDL program in behavioral model for flip flops, shift registers and counters/Counters/down_counter/counter_down4bit.v
@@ -0,0 +1,14 @@
+module counter_down4bit(clk,rst,count);
+  
+  input clk,rst;
+  output reg [3:0]count;
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        count<=0;
+      else
+        count<=count-1'b1;
+    end
+  
+  endmodule
