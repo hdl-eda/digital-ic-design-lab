1046228ac6fc07302ba9ac82fcf8c6c3dc50a261
Mohammed Zakir Hussain <127012447+hdl-eda@users.noreply.github.com>
2023-03-13 15:17:05 +0530

    Create tb_ud_counter4bit.v

---
+++ part-a experiments/4 Verilog HDL program in behavioral model for flip flops, shift registers and counters/Counters/up_down_counter/tb_ud_counter4bit.v
@@ -0,0 +1,32 @@
+module tb;
+  
+  reg clk,rst;
+  reg ud; // ud=1 for up count; ud=0 for down count
+  wire [3:0]count;
+ 
+  counter_updown4bit dut(clk,rst,ud,count);
+  
+  
+  initial
+    begin
+      clk<=0;
+      rst<=0;
+    end
+  
+  always #5 clk<=~clk;
+  
+  initial
+    begin
+      $monitor("clk %b,rst %b,ud %b,count %0d",clk,rst,ud,count);
+      @(posedge clk); rst<=1;
+      @(posedge clk); rst<=0;
+      repeat(20)
+        begin
+          @(posedge clk); ud<=1;
+        end
+       repeat(20)
+        begin
+          @(posedge clk); ud<=0;
+        end
+      $stop;
+    end
