34bc93f141b76f4a5b858545205dc009b27a8b58
Mohammed Zakir Hussain <127012447+hdl-eda@users.noreply.github.com>
2025-07-21 20:09:17 +0530

    Create tb_moore_fsm.v

---
+++ part-a experiments/6 Verilog HDL program for 4 bit sequence detector moore/4 bit sequence detector through Moore state machines/tb_moore_fsm.v
@@ -0,0 +1,42 @@
+```
+// Testbench 
+
+module TB;
+  reg clk, rst_n, x;
+  wire z;
+  
+  seq_detector_1010 sd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
+```
