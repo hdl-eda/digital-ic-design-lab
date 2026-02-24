99bfbad5f4a2278fc286038a74397fb5dca624ef
Mohammed Zakir Hussain <127012447+hdl-eda@users.noreply.github.com>
2026-02-24 11:55:41 +0530

    Update tb_rca4bit.v

--- part-a experiments/1 Structural and dataflow Verilog HDL models/a) 4-bit ripple carry adder/tb_rca4bit.v
+++ part-a experiments/1 Structural and dataflow Verilog HDL models/a) 4-bit ripple carry adder/tb_rca4bit.v
@@ -6,26 +6,41 @@ module TestModule;
 // Inputs
 reg [3:0] a;
 reg [3:0] b;
-reg cin;
 
 // Outputs
 wire [3:0] sum;
 wire cout;
 
 // Instantiate the Unit Under Test (UUT)
-adder_4bit dut(a,b,cin,sum,cout);
+adder_4bit dut(a,b,sum,cout);
 initial begin
-  $monitor("a=%0d b=%0d cin=%b  sum=%0d cout=%b",a,b,cin,sum,cout);
+  $monitor("a=%0d b=%0d cout=%b sum=%0d", a, b, cout, sum);
+
+  // The following takes only two sets of inputs
+  
 // Initialize Inputs
 a = 0;
 b = 0;
-cin = 0;
-// Wait 100 ns for global reset to finish
-#100;
+
+// Wait 10ns for global reset to finish
+#10;
 a = 5;
 b = 6;
-cin = 1;
-// Wait 100 ns for global reset to finish
-#100;
+
+// Wait 10ns for global reset to finish
+#10;
+$stop;
+  
+  //The other way of testing: following uses a for-loop with 16 different inputs
+  /*
+  for(i=0; i<=15; i=i+1) begin
+  a=$random%15;
+  b=$random%15;
+  $5;
+  end
+  #5;
+  $stop;
+  */
+  
 end
 endmodule
